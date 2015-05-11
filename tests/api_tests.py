44c998439a0b4a49f94d4d55905321df18961129
Des Penny <despenny@gmail.com>
2015-05-11 03:10:01 +0000

    Test to get songs from a populated DB

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -47,3 +47,41 @@ class TestAPI(unittest.TestCase):
 
         data = json.loads(response.data)
         self.assertEqual(data, [])    
+
+    def testGetSong(self):
+        """ Getting songs from a populated database """
+        fileA = models.File(filename="FileA.mp3")
+        fileB = models.File(filename="FileB.mp3")
+
+        session.add_all([fileA, fileB])
+        session.commit()
+        
+        songA = models.Song(song_file_id= fileA.id)
+        songB = models.Song(song_file_id= fileB.id)
+
+        session.add_all([songA, songB])
+        session.commit()
+        
+        response = self.client.get("/api/songs",
+                                   headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+        
+        data=json.loads(response.data)
+        
+        # Test number of songs
+        songs = session.query(models.Song).all()
+        self.assertEqual(len(songs), 2)
+
+        songA = data[0]
+        self.assertEqual(songA["id"], songA["file"]["id"])
+        self.assertEqual(songA["file"]["name"], "FileA.mp3")
+
+        songB = data[1]
+        self.assertEqual(songB["id"], songB["file"]["id"])
+        self.assertEqual(songB["file"]["name"], "FileB.mp3")
+        
+if __name__ == "__main__":
+    unittest.main()
